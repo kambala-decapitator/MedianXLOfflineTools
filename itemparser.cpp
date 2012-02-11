2b61cb5ff137e7d6a31037291d082e6633d813a9
kambala <decapitator@ukr.net>
2012-02-12 00:44:58 +0200

    updated russian data for 2012 004

--- itemparser.cpp
+++ itemparser.cpp
@@ -195,7 +195,7 @@ ItemInfo *ItemParser::parseItem(QDataStream &inputDataStream, const QByteArray &
                 ItemInfo *socketableInfo = parseItem(inputDataStream, bytes);
                 item->socketablesInfo += socketableInfo;
                 if (item->isRW && i >= item->socketablesNumber - 2) // get the last socket filler to obtain RW name (and previous one to prevent disambiguation)
-                    runeTypes.prepend(/*i != item->socketablesNumber - 1*/socketableInfo->itemType == "jew" ? QByteArray() : socketableInfo->itemType);
+                    runeTypes.prepend(socketableInfo->itemType == "jew" ? QByteArray() : socketableInfo->itemType);
             }
 
             if (runeTypes.size())
