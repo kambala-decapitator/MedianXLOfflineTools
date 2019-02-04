087b09f5efddf7114b878bc8f5d90531cb331089
Andrey Filipenkov <decapitator@ukr.net>
2019-02-05 01:05:41 +0300

    update skill script

--- scripts/skill_icons.pl
+++ scripts/skill_icons.pl
@@ -10,9 +10,9 @@ my $skillIconsDir = $ARGV[0];
 opendir(my $dh, $skillIconsDir) || die "error opening dir: $!";
 chdir $skillIconsDir;
 
-my %characterIndexes = (am => 0, so => 1, ne => 2, pa => 3, ba => 4, dr => 5, as => 6);
+my %characterIndexes = (ama => 0, sor => 1, nec => 2, pal => 3, bar => 4, dru => 5, ass => 6);
 while (readdir $dh) {
-    next unless /^([a-z]{2})skillicon_/;
+    next unless /^([a-z]{3})_icons_/;
     my $idx = $characterIndexes{$1};
     my $outDir = "$skillIconsDir/$idx";
     mkdir $outDir;
