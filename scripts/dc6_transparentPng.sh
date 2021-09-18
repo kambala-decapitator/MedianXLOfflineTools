c6163bd381701ecc81707c5b485bef6b82b58b1d
Andrey Filipenkov <decapitator@ukr.net>
2021-09-18 09:10:10 +0300

    update dc6con path

--- scripts/dc6_transparentPng.sh
+++ scripts/dc6_transparentPng.sh
@@ -3,7 +3,7 @@
 rm -f *.pcx
 
 for f in $(find . -iname '*.dc6')
-    ~/Documents/MXL/dc6con.zip\ Folder/dc6con "$f"
+    ~/Diablo\ II\ save\ files/dc6con.zip\ Folder/dc6con "$f"
 
 mogrify -format png -fuzz 2% -transparent black *.pcx
 # mogrify -format png *.pcx
