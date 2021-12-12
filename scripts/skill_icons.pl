c148175977e4a88a208742f59eea4d5ceb312e58
Andrey Filipenkov <decapitator@ukr.net>
2021-12-12 11:31:45 +0300

    update skill icons script to handle output of qdc6

--- scripts/skill_icons.pl
+++ scripts/skill_icons.pl
@@ -12,15 +12,14 @@ chdir $skillIconsDir;
 
 my %characterIndexes = (ama => 0, sor => 1, nec => 2, pal => 3, bar => 4, dru => 5, ass => 6);
 while (readdir $dh) {
-    next unless /^([a-z]{3})_icons_/;
+    next unless /^([a-z]{3})_icons$/;
     my $idx = $characterIndexes{$1};
     my $outDir = "$skillIconsDir/$idx";
     mkdir $outDir;
-    
-    while (my $icon = <$skillIconsDir/$_/00/*>) {
+
+    while (my $icon = <$skillIconsDir/$_/*>) {
         my $newName = basename($icon);
         next if $newName =~ /[13579]\./;
-        $newName =~ s/^0{0,3}//;
         copy($icon, "$outDir/$newName");
     }
 }
