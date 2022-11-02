72a60c421c84f1d1859a91511e6ceba398d24d39
Andrey Filipenkov <decapitator@ukr.net>
2022-11-02 18:26:49 +0300

    allow multiple rsync options in the mod data script

--- scripts/distribute_mod_data.sh
+++ scripts/distribute_mod_data.sh
@@ -2,9 +2,9 @@
 
 scriptDir=$(cd "$(dirname "$0")" && pwd)
 zipName="$1.zip"
-extraExclude=$2
+extraRsyncOptions="$2"
 
 mkdir resources
-rsync "$scriptDir/../resources/data" -qr --exclude=data/items --exclude=data/sorting $extraExclude resources
+rsync "$scriptDir/../resources/data" -qr --exclude=data/items --exclude=data/sorting $extraRsyncOptions resources
 zip -qr "$zipName" resources
 rm -rf resources
